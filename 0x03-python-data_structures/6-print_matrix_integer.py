4c08d857b70c48ea6730c109a4edd6418c31a91d
Mohamed <ph.dude.95@gmail.com>
2024-01-08 02:57:33 +0200

    w

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,4 @@ def print_matrix_integer(matrix=[[]]):
         if len(ins) == 0:
             print()
         for i in range(len(ins)):
-            print("{:d}".format(ins[i]),
-                    end="\n" if i is len(ins) - 1 else " ")
+            print("{:d}".format(ins[i]),end="\n" if i is len(ins) - 1 else " ")
