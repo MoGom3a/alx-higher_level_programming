64fbedfb7cf36e511656136d12168f667998bf79
Mohamed <ph.dude.95@gmail.com>
2024-01-08 03:33:38 +0200

    8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
+    if sentence:
+       leng = len(sentence)
+    else:
+        leng = 0
+    return (leng, sentence if not sentence else sentence[:1])
+
