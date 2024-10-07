6b5f1fea04d037bc01682aace95af549e3bc1ed0
Mohamed Gomaa <Mohamed.g.abdelraouf@gmail.com>
2024-10-07 03:29:14 +0300

    s

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to convert a Roman numeral to an integer
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_n = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]]
-        else:
-            roman_n += roman_d[roman_string[j]]
-    return roman_n
+    if type(roman_string) == str:
+        sum_v = 0
+        num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        for i in range(len(roman_string)):
+
+            if i == len(roman_string) - 1:
+                sum_v += num[roman_string[i]]
+
+            elif num[roman_string[i + 1]] <= num[roman_string[i]]:
+                sum_v += num[roman_string[i]]
+
+            else:
+                sum_v -= num[roman_string[i]]
+
+        return (sum_v)
+    else:
+        return (0)
